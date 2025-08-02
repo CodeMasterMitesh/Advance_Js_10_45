bbae8cae5d67f6b8d0cb46af4d0a709509f29d49
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-08-02 11:47:16 +0530

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+// An IIFE, or Immediately Invoked Function Expression, in JavaScript is a function that is defined and executed immediately after its creation. This pattern is commonly used to create a private scope for variables, preventing them from polluting the global scope and enabling more controlled and modular code organization
+// function notification()
+// {
+//   alert("Good Morning. Welcome To My Website");
+// }
+// notification();
+
+// (function(){
+//     alert("Good Morning. Welcome To My Website");
+// })();
+
+
+// arrow function 
+ 
+// let notification = ()=>alert("Good Morning. Welcome To My Website");
+// notification();
